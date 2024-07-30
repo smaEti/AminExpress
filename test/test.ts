fbb9d2503004d9154c9ff8c5dded5bbc0efe7762
Amin Ehterami <A.Ehterami@proton.me>
2024-07-30 16:45:27 +0330

    test environment

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,55 @@
 import { URL } from "node:url";
 import querystring from "node:querystring";
-const myURL = new URL(
-  "https://user:pass@sub.example.com:8080/p/a/t/h?query=string&foo=bar&abc=xyz&abc=123#hash"
-);
-console.log(querystring.parse(myURL.searchParams.toString()));
 
+// const myURL = new URL(
+//   "https://user:pass@sub.example.com:8080/p/a/t/h?query=string&foo=bar&abc=xyz&abc=123#hash"
+// );
+// console.log(querystring.parse(myURL.searchParams.toString()));
+const fs = require('fs');
+const path = require('path');
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext;
+  // maps file extension to MIME typere
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+  };
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory search for index file matching the extension
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
